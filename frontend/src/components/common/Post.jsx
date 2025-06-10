f4632a1580e9e21b6c80e893518092ee47e968a6
KaizenPrad <Namandwivedi32@gmail.com>
2025-06-10 21:02:41 +0530

    POSTS Delete Commit

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -5,22 +5,51 @@ import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import LoadingSpinner from "./LoadingSpinner";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
+	const {data:authUser} = useQuery({queryKey:["authUser"], queryFn:()=>fetch("/api/auth/me").then(res=>res.json())} );
+	const queryClient = useQueryClient();
 	const postOwner = post.user;
 	const comments = post.comments || [];
 	const likes = post.likes || [];
 
+	const {mutate:deletePost, isPending}= useMutation({
+		mutationFn: async()=>{
+			try {
+				const res = await fetch(`/api/posts/${post._id}`,{
+					method: "DELETE",
+				});
+				const data = await res.json();
+
+				if(!res.ok) {
+					throw new Error(data.error || "Something went wrong ");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess:()=>{
+			toast.success("Post deleted successfully",);
+			queryClient.invalidateQueries({queryKey:["posts"]});
+		}
+	});
+
 	const isLiked = false;
 
-	const isMyPost = true;
+	const isMyPost = authUser._id===post.user._id;
 
 	const formattedDate = "1h";
 
 	const isCommenting = false;
 
-	const handleDeletePost = () => {};
+	const handleDeletePost = () => {
+		deletePost();
+	};
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
@@ -48,8 +77,11 @@ const Post = ({ post }) => {
 						</span>
 						{isMyPost && (
 							<span className='flex justify-end flex-1'>
-								<FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />
+								{!isPending && <FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />} 
+
+								{isPending && (<LoadingSpinner size="sm"/>)}
 							</span>
+
 						)}
 					</div>
 					<div className='flex flex-col gap-3 overflow-hidden'>
