437572051840118829eeaba87ca2b5c1562095f2
KaizenPrad <Namandwivedi32@gmail.com>
2025-06-11 20:29:42 +0530

    Last Development Things

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import {v2 as cloudinary} from "cloudinary";
@@ -20,6 +21,7 @@ import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+const __dirname = path.resolve();
 
 
 app.use(express.json({ limit: '10mb' })); // default is 100kb
@@ -40,6 +42,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes)
+
+if(process.env.NODE_ENV === "production")
+{ app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+
+}
 
 app.listen(PORT , () => {
   console.log("Server is running on port " + PORT);
