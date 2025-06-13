b1eaa52c12b243be601d2b4fa90610a6587bdcbf
KaizenPrad <Namandwivedi32@gmail.com>
2025-06-13 13:39:13 +0530

    Made api Changes

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,6 +2,7 @@ const BASE = import.meta.env.VITE_API_URL;
 
 export const apiFetch = (path, options = {}) => {
   return fetch(`${BASE}${path}`, {
+     credentials: "include",
     ...options,
     headers: {
       'Content-Type': 'application/json',
