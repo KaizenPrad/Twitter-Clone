ed94e6c123eed24db30912e46127d6e90d0b38e0
KaizenPrad <Namandwivedi32@gmail.com>
2025-06-13 11:24:57 +0530

    singup page link changed

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -22,7 +22,7 @@ const SignUpPage = () => {
   const { mutate, isError, isPending, error } = useMutation({
     mutationFn: async ({ email, username, fullName, password }) => {
       try {
-        const res = await fetch("/api/auth/signup", {
+        const res = await fetch("https://twitter-clone-main-ibbb.onrender.com/api/auth/signup", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
