451ea7256df89348fdd885e76ff5626e51a35795
KaizenPrad <Namandwivedi32@gmail.com>
2025-06-13 10:53:18 +0530

    Vite Config change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://twitter-clone-main-ibbb.onrender.com',
         changeOrigin: true,
       }
     }
