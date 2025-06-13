39d1a5ab5297ef57e447bb73496df07766a86516
KaizenPrad <Namandwivedi32@gmail.com>
2025-06-13 13:02:36 +0530

    made server changes

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://twitter-clone-main-ibbb.onrender.com'
+  'https://twitter-clone-tan-two.vercel.app'
 ];
 
 app.use(cookieParser());
